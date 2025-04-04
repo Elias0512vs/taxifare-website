71443af78ad9f5c161b31808dc8f3545aa03f09b
Elias Navarro <elias1621tc@gmail.com>
2025-04-04 13:53:27 -0500

    My first website_ 2 pages

---
+++ app/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime,date
+import requests
+
+'''
+# TaxiFareModel front
+'''
+
+st.markdown('''
+ Welcome to the app
+''')
